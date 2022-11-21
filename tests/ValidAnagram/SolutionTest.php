a93985b1c6ef216de96f037c031a6633c364e55a
ktunprasert <ktunprasert@outlook.com>
2022-11-21 22:56:04 +0000

    validanagram: more unit tests

--- tests/ValidAnagram/SolutionTest.php
+++ tests/ValidAnagram/SolutionTest.php
@@ -15,10 +15,27 @@ class ValidAnagramTest extends TestCase
         $this->assertTrue($solver->isAnagram($first, $second));
     }
 
+    /** @dataProvider invalidAnagrams */
+    public function test_it_fails_invalid_anagrams(string $first, string $second): void
+    {
+        $solver = new Solution();
+
+        $this->assertFalse($solver->isAnagram($first, $second));
+    }
+
     public function validAnagrams(): array
     {
         return [
             ["anagram", "gramana"],
+            ["cac", "acc"],
+        ];
+    }
+
+    public function invalidAnagrams(): array
+    {
+        return [
+            ["cat", "dog"],
+            ["bottle", "water"],
         ];
     }
 }
